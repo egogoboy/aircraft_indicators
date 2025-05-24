42f02a7ab6e6030a38671de421cc628c33ac1c74
egogoboy <egor2005sudakov@gmail.com>
2025-05-24 17:24:19 +0300

    Updated __init__.py

---
+++ aircraft_indicators/__init__.py
@@ -0,0 +1,11 @@
+from .heading import HeadingIndicator
+from .attitude import AttitudeIndicator
+from .drift import DriftAngleIndicator
+from .speed import SpeedIndicator
+
+__all__ = [
+    "HeadingIndicator",
+    "AttitudeIndicator",
+    "DriftAngleIndicator",
+    "SpeedIndicator"
+]
